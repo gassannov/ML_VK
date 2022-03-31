c065e7c82c81ea3b22a1afabc631804066db3a30
Marat Gasanov <maratgasanov6@gmail.com>
2022-03-31 20:44:30 +0300

    format code

--- dz2s/dz2_lib.c
+++ dz2s/dz2_lib.c
@@ -1,18 +1,22 @@
 #include "dz2_lib.h"
 
-int max_word(const char* str, size_t str_len){
+int max_word(const char *str, size_t str_len)
+{
     size_t curr_len = 0;
     size_t max_word = 0;
     int is_word = 0;
 
     for (size_t j = 0; j < str_len; ++j)
     {
-        if ((str[j] >= 'A' && str[j] <= ('A' + 25)) || (str[j] >= 'a' && str[j] <= ('a' + 25)) ){
-            is_word = 1; 
+        if ((str[j] >= 'A' && str[j] <= ('A' + 25)) || (str[j] >= 'a' && str[j] <= ('a' + 25)))
+        {
+            is_word = 1;
             curr_len++;
         }
-        else if(str[j] == ' ' || str[j] == '\n'){
-            if(is_word){
+        else if (str[j] == ' ' || str[j] == '\n')
+        {
+            if (is_word)
+            {
                 if (curr_len > max_word)
                 {
                     max_word = curr_len;
@@ -21,12 +25,14 @@ int max_word(const char* str, size_t str_len){
             curr_len = 0;
             is_word = 0;
         }
-        else{
+        else
+        {
             curr_len = 0;
             is_word = 0;
         }
     }
-    if(curr_len > max_word){
+    if (curr_len > max_word)
+    {
         max_word = curr_len;
     }
     return max_word;
--- main.c
+++ main.c
@@ -7,32 +7,34 @@
 #include <sys/stat.h>
 #include "string.h"
 #include <fcntl.h>
-#include <time.h> 
+#include <time.h>
 #include "dz2_lib.h"
 
 #define ARR_SIZE 100000000
 #define FORKS_NUMBER 10
 
-int main() {
+int main()
+{
 
-// VAR INITIALIZE AND DATA PREPARING
+    // VAR INITIALIZE AND DATA PREPARING
 
     const char *filename = "../large.txt";
 
     int fd = open(filename, O_RDWR); // или O_RDWR
     struct stat st;
     stat(filename, &st);
-    
+
     size_t file_size = st.st_size;
 
     char *shared_memory = mmap(NULL,
-                        file_size,
-                        PROT_READ | PROT_WRITE,
-                        MAP_PRIVATE,
-                        fd,
-                        0);
+                               file_size,
+                               PROT_READ | PROT_WRITE,
+                               MAP_PRIVATE,
+                               fd,
+                               0);
 
-    if (shared_memory == MAP_FAILED) {
+    if (shared_memory == MAP_FAILED)
+    {
         printf("mmap failed\n");
         close(fd);
         return 1;
